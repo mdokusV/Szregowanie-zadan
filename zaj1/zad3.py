3b24cd80e6df4ce194d51095b4127061e8846216
HOME-VM-TOSCHOOL <mat.dok123@gmail.com>
2024-03-06 20:17:45 +0100

    zaj1 zad3

--- zaj1/zad3.py
+++ zaj1/zad3.py
@@ -12,21 +12,73 @@ sys.stdin = io.StringIO(input_string)
 
 size = int(input())
 
+
+class Task:
+    def __init__(self, relies_time, processing_time, index):
+        self.relies_time = relies_time
+        self.processing_time = processing_time
+        self.index = index
+
+    def __lt__(self, other):
+        return (self.processing_time, self.index) < (other.processing_time, other.index)
+
+    def __gt__(self, other):
+        return (self.processing_time, self.index) > (other.processing_time, other.index)
+
+    def __eq__(self, other):
+        return (self.processing_time, self.index) == (
+            other.processing_time,
+            other.index,
+        )
+
+    def __str__(self):
+        return f"{self.relies_time} {self.processing_time} {self.index}"
+
+
 from queue import PriorityQueue
 
-pq = PriorityQueue()
 
-relies_table = []
+relies_table: list[Task] = []
 timestamps = []
 for i in range(size):
-    for line in input().splitlines():
-        relies_time, processing_time = map(int, line.split())
-        relies_table.append((relies_time, processing_time))
-        timestamps.append(relies_time)
+    relies_time, processing_time = map(int, input().split())
+    relies_table.append(Task(relies_time, processing_time, i))
+    timestamps.append(relies_time)
 
-print(relies_table)
-pq.put(relies_table.pop(0))
-time = 0
-while pq.qsize() > 0:
-    processing = pq.get()
-    was_processed = relies_table[i + 1][0]
+solution = [0 for _ in range(size)]
+
+pq: PriorityQueue[Task] = PriorityQueue()
+
+
+while len(timestamps) >= 2:
+    time = timestamps.pop(1)
+    new_task = relies_table.pop(0)
+    pq.put(new_task)
+    current_task = pq.get()
+    while current_task.relies_time < time:
+        if current_task.processing_time < time - current_task.relies_time:
+            time_passed = current_task.processing_time + current_task.relies_time
+            solution[current_task.index] = time_passed
+
+            if not pq.empty():
+                current_task = pq.get()
+                current_task.relies_time = time_passed
+            else:
+                break
+            continue
+
+        current_task.processing_time = current_task.processing_time - (
+            time - current_task.relies_time
+        )
+        current_task.relies_time = time
+        pq.put(current_task)
+
+pq.put(relies_table[0])
+while not pq.empty():
+    current_task = pq.get()
+    time = time + current_task.processing_time
+    solution[current_task.index] = time
+
+for i in solution:
+    print(i)
+print(solution)
