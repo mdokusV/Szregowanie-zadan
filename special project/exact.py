753916d1421a28d014a8ca499d6697ba84532143
HOME-VM-TOSCHOOL <mat.dok123@gmail.com>
2024-06-20 02:50:39 +0200

    zaj11

--- special project/exact.py
+++ special project/exact.py
@@ -1,15 +1,22 @@
+import copy
 import random
 
+import scipy
+from sympy import Integer
+
+
 MAX_TIME = 10
+RANDOM_CONNECTION = True
 
 class Task:
     def __init__(self, index: int, processing_time: int):
         self.index = index
         self.processing_time = processing_time
         self.setups: list[Setup] = []
+        self.required_importance = 0.0
 
     def __str__(self):
-        return f"task {self.index}, processing time: {self.processing_time}, setups: {[str(setup) for setup in self.setups]}"
+        return f"task {self.index}, processing: {self.processing_time}, required: {self.required_importance}, setups: {sorted([str(setup.index) for setup in self.setups])}"
 
 
 class Setup:
@@ -17,9 +24,10 @@ class Setup:
         self.index = index
         self.processing_time = processing_time
         self.tasks: list[Task] = []
+        self.importance = 0.0
 
     def __str__(self):
-        return f"setup {self.index}, processing time: {self.processing_time}"
+        return f"setup {self.index}, processing: {self.processing_time},  importance: {self.importance}, tasks: {sorted([str(task.index) for task in self.tasks])}"
 
     def __hash__(self):
         return self.index
@@ -34,26 +42,49 @@ class Machine:
         self.time = 0
         self.tasks_done: list[Task] = []
         self.setups_done: set[Setup] = set()
+        self.summed_processing: int = 0
 
     def sum_time(self) -> int:
         time_sum = sum(task.processing_time for task in self.tasks_done)
         time_sum += sum(setup.processing_time for setup in self.setups_done)
+        self.summed_processing = time_sum
         return time_sum
 
+    def copy_order(self, other: "Machine"):
+        self.tasks_done = copy.deepcopy(other.tasks_done)
+        self.setups_done = copy.deepcopy(other.setups_done)
+        self.summed_processing = other.summed_processing
+
     def __str__(self):
-        return f"machine {self.index}, tasks done: {[str(task) for task in self.tasks_done]}, setups done: {[str(setup) for setup in self.setups_done]}"
+        return f"machine {self.index}, summed time: {self.summed_processing}, tasks done: {sorted([str(task.index) for task in self.tasks_done])}, setups done: {sorted([str(setup.index) for setup in self.setups_done])}"
 
 
-task_number, setup_number, machine_number = 3, 2, 2
+task_number, setup_number, machine_number = 10, 10, 3
 
-connection_matrix = [
-    [random.choice([0, 1]) for _ in range(setup_number)] for _ in range(task_number)
-]
+
+def random_connection(task_number, setup_number):
+    connection_matrix = [[0 for _ in range(setup_number)] for _ in range(task_number)]
+    for row in connection_matrix:
+        random_len = random.randint(1, len(row))
+        places_for_one = random.sample(range(len(row)), random_len)
+        for place in places_for_one:
+            row[place] = 1
+    return connection_matrix
+
+
+if RANDOM_CONNECTION:
+    connection_matrix = random_connection(task_number, setup_number)
+else:
+    connection_matrix = [
+        [0, 1, 1],
+        [1, 1, 1],
+        [1, 0, 0],
+    ]
 
 
 tasks = [Task(i, random.randint(1, MAX_TIME)) for i in range(task_number)]
 
-setups = [Setup(i, random.randint(1, MAX_TIME)) for i in range(machine_number)]
+setups = [Setup(i, random.randint(1, MAX_TIME)) for i in range(setup_number)]
 
 machines = [Machine(i) for i in range(machine_number)]
 
@@ -66,10 +97,24 @@ for task_ind, task_con in enumerate(connection_matrix):
             tasks[task_ind].setups.append(setups[set_ind])
             setups[set_ind].tasks.append(tasks[task_ind])
 
-print(*tasks, sep="\n")
+
+for setup in setups:
+    setup.importance = (
+        scipy.stats.pmean(
+            [task.processing_time for task in setup.tasks],
+            3,
+        )
+        * setup.processing_time
+    )
+
+for task in tasks:
+    task.required_importance = sum(setup.importance for setup in task.setups)
+
+print(*tasks, sep="\n", end="\n\n")
+print(*setups, sep="\n", end="\n\n")
 
 
-def get_permutation(n):
+def get_permutation(n: int, base: int):
     binary_list = [not (bool(int(i))) for i in list(bin(n)[2:].zfill(len(tasks)))]
     return [tasks[i] for i, v in enumerate(binary_list) if v == True], [
         tasks[i] for i, v in enumerate(binary_list) if v == False
@@ -106,7 +151,8 @@ def find_best() -> tuple[list[Machine], int]:
             # )
         if max(times) < minimal_summed_time:
             minimal_summed_time = max(times)
-            best_arrangement = machines.copy()
+            for i, machine in enumerate(best_arrangement):
+                machine.copy_order(machines[i])
 
         permutation_number += 1
 
@@ -115,5 +161,6 @@ def find_best() -> tuple[list[Machine], int]:
 
 best_arrangement, minimal_summed_time = find_best()
 
+print()
 print(*best_arrangement, sep="\n")
 print(minimal_summed_time)
---
+++ zaj11/zad1.py
@@ -0,0 +1,153 @@
+import io
+import sys
+
+
+input_string = """10 10
+70 60 50 52 98 75 27 78 9 3 
+33 41 2 31 14 69 10 21 63 52 
+69 15 66 6 84 89 84 58 86 89 
+91 100 17 32 92 25 3 17 13 80 
+54 83 57 42 85 92 8 53 24 39 
+20 96 10 49 64 6 8 75 76 60 
+13 99 81 36 96 98 94 27 46 81 
+19 69 13 18 94 60 44 28 28 50 
+91 58 25 71 73 21 13 22 68 49 
+24 10 33 65 33 37 49 37 40 72 
+"""
+sys.stdin = io.StringIO(input_string)
+
+
+from math import exp
+import copy
+from dataclasses import dataclass, field
+import random
+from typing import List
+
+RNG = random.Random()
+
+
+class Param:
+    def __init__(self):
+        self.ITER_MAX = 1000
+        self.temperature = 50
+        self.FREEZING = 0.875
+
+    def update_temp(self):
+        self.temperature = self.temperature * self.FREEZING
+
+
+PARAM = Param()
+
+
+@dataclass
+class Task:
+
+    index: int
+    processing_times: List[int] = field(default_factory=list)
+    sum: int = 0
+
+    def __str__(self):
+        return f"{self.index}"
+
+    def __lt__(self, other: "Task"):
+        return self.sum > other.sum
+
+    def __eq__(self, other: "Task"):
+        return self.sum == other.sum
+
+    def __gt__(self, other: "Task"):
+        return self.sum < other.sum
+
+
+@dataclass
+class Order:
+    tasks: List[Task]
+    sum: int = 0
+
+    def __str__(self):
+        return f"{self.sum}\n" + " ".join([str(task) for task in self.tasks])
+
+    def calculate_sum(self) -> int:
+        times_of_workers = [0 for _ in range(worker_number)]
+        for task in self.tasks:
+            for worker in range(worker_number):
+                if worker == 0:
+                    times_of_workers[worker] += task.processing_times[worker]
+                else:
+                    times_of_workers[worker] = (
+                        max(times_of_workers[worker], times_of_workers[worker - 1])
+                        + task.processing_times[worker]
+                    )
+
+        self.sum = times_of_workers[-1]
+        return times_of_workers[-1]
+
+    def copy_order(self, other: "Order"):
+        self.tasks = copy.deepcopy(other.tasks)
+        self.sum = other.sum
+
+    def local_search(self, best_order: "Order") -> int:
+        old_sum = self.sum
+
+        take = RNG.randint(0, len(self.tasks) - 1)
+        swap = RNG.randint(1, len(self.tasks) - 1)
+        swap_index = (take + swap) % len(self.tasks)
+        (
+            self.tasks[take],
+            self.tasks[swap_index],
+        ) = (
+            self.tasks[swap_index],
+            self.tasks[take],
+        )
+        self.calculate_sum()
+
+        if self.sum < old_sum:
+            if self.sum < best_order.sum:
+                best_order.copy_order(self)
+            return 0
+
+        if -exp((old_sum - self.sum) / PARAM.temperature) > RNG.random():
+            if self.sum < best_order.sum:
+                best_order.copy_order(self)
+            return 0
+
+        (
+            self.tasks[take],
+            self.tasks[swap_index],
+        ) = (
+            self.tasks[swap_index],
+            self.tasks[take],
+        )
+        PARAM.update_temp()
+        return 1
+
+    def __hash__(self) -> int:
+        return hash(tuple(task.index for task in self.tasks))
+
+    def __eq__(self, other: "Order") -> bool:
+        return self.tasks == other.tasks
+
+
+worker_number, task_number = map(int, input().split())
+
+tasks = [Task(i) for i in range(task_number)]
+
+for i in range(task_number):
+    tasks[i].processing_times = list(map(int, input().split()))
+    tasks[i].sum = sum(tasks[i].processing_times)
+
+tasks.sort()
+
+
+best_order = Order(tasks)
+best_order.calculate_sum()
+
+order = Order(tasks)
+order.calculate_sum()
+
+missis = 0
+while missis < PARAM.ITER_MAX:
+    missis += order.local_search(best_order)
+
+best_order.calculate_sum()
+print(best_order)
